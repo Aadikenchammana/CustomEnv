6a59355b5e85c1a235ff72f188ba9895b467ce6b
Aadi Kenchammana <104882666+Aadikenchammana@users.noreply.github.com>
2024-02-16 18:54:45 -0800

    tweaking to improve training efficacy

--- customSBenv.py
+++ customSBenv.py
@@ -11,6 +11,14 @@ import os
 from datetime import datetime
 import math
 
+def save_array_to_file(array, filename):
+
+    # Convert array to string with custom format
+    array_str = '|'.join(','.join(str(cell) for cell in row) for row in array)
+    # Save string to file
+    with open(filename, 'w') as file:
+        file.write(array_str)
+    
 
 def get_burning(mp):
     count = 0
@@ -51,7 +59,7 @@ def get_reward(mp):
 
 def get_reward_l2(mp, x, y):
     dist, square = distance_to_fire(mp,x,y)
-    return -50*(get_burning(mp)+get_burned(mp))/get_total(mp) - 0.2*dist
+    return -10*(get_burning(mp)+get_burned(mp))/get_total(mp) - 0.5*dist
 
 def run_one_simulation_step(self, total_updates):
     num_updates = 0
@@ -113,10 +121,12 @@ class CustomEnv(gym.Env):
         self.agent_start = [10,10]
         self.episode_steps = 0
         self.updates_per_step = 1
-        self.total_steps_per_episode = 200
-        self.fire_restart_threshold = 300
+        self.total_steps_per_episode = 300
+        self.episodes_per_fire_restart = 150
         self.chkpt_thresh = 20
+        self.simulation_steps_per_timestep = 8
         self.episode_num = 0
+        self.autoplace = True
 
         self.analytics_dir = "train_analytics//"+datetime.now().strftime("%m.%d.%Y_%H:%M:%S")
         if os.path.isdir(self.analytics_dir) == False:
@@ -134,9 +144,15 @@ class CustomEnv(gym.Env):
         self.chkpt_dir = self.analytics_dir+"//fires//0"
         os.mkdir(self.chkpt_dir)
 
-        n_actions = 5
-        self.action_space = spaces.Discrete(n_actions)
-        self.action_names = ["up","down","left","right","fireline"]
+        
+        if self.autoplace:
+            n_actions = 4
+            self.action_space = spaces.Discrete(n_actions)
+            self.action_names = ["up","down","left","right"]
+        else:
+            n_actions = 5
+            self.action_space = spaces.Discrete(n_actions)
+            self.action_names = ["up","down","left","right","fireline"]
         n_channel = 1
         self.observation_space = spaces.Box(low=0, high=255,shape=(n_channel, self.screen_size, self.screen_size), dtype=np.int64)
 
@@ -144,7 +160,7 @@ class CustomEnv(gym.Env):
     def step(self, action):
         self.episode_steps += 1
         action_str = self.action_names[action]
-        action_multiplier = 3
+        action_multiplier = 1
 
         with open(self.analytics_dir+"//customLog.txt","a") as f:
             f.write("\n ACTION PREFORMED, "+action_str+","+str(self.agent_x)+","+str(self.agent_y))
@@ -168,15 +184,22 @@ class CustomEnv(gym.Env):
         
         if action_str == "fireline":
             self.sim.update_mitigation([(self.agent_x,self.agent_y,BurnStatus.FIRELINE)])
+        
+        if self.autoplace:
+            self.sim.update_mitigation([(self.agent_x,self.agent_y,BurnStatus.FIRELINE)])
 
-        #self.fire_map, self.fire_status = self.sim.run("10m")
-        self.fire_map, self.fire_status = run_one_simulation_step(self, self.updates_per_step)
+        if self.episode_steps%self.simulation_steps_per_timestep == 0:
+            self.fire_map, self.fire_status = run_one_simulation_step(self, self.updates_per_step)
 
 
         self.observation = self.fire_map[newaxis,:,:]
         terminated = False
+        truncated = False
         if self.episode_steps > self.total_steps_per_episode:
             terminated = True
+        if get_burning(self.fire_map) == 0:
+            terminated = True
+            truncated = False
         reward = get_reward_l2(self.fire_map, self.agent_x, self.agent_y)#get_reward(self.fire_map)
 
         with open(self.analytics_dir+"//customLog.txt","a") as f:
@@ -186,9 +209,9 @@ class CustomEnv(gym.Env):
             f.write("\n REWARD, "+str(reward)+","+str(get_burned(self.fire_map))+","+str(get_burning(self.fire_map))+","+str(get_unburned(self.fire_map))+","+str(distance_to_fire(self.fire_map,self.agent_x,self.agent_y)[0]))
 
         if self.chkpt_flag:
+            #save_array_to_file(self.fire_map, self.chkpt_dir+"//"+str(self.episode_steps)+".txt")
             np.save(self.chkpt_dir+"//"+str(self.episode_steps)+".npy",self.fire_map)
-
-        truncated = False
+        
         info = {}
         return self.observation, reward, terminated, truncated, info
 
@@ -199,13 +222,15 @@ class CustomEnv(gym.Env):
             self.chkpt_flag = True
             self.chkpt_dir = self.analytics_dir+"//fires//"+str(int(self.episode_num/self.chkpt_thresh))
             os.mkdir(self.chkpt_dir)
-        if self.episode_num%self.fire_restart_threshold == 0:
+        if self.episode_num%self.episodes_per_fire_restart == 0:
             self.config.fire.fire_initial_position = calc_random_start(self.config.area.screen_size[0])
         self.sim = simfire.sim.simulation.FireSimulation(self.config)
         self.sim.reset()
         self.fire_map, self.fire_status = run_one_simulation_step(self, 1)
         self.observation_return = self.fire_map[newaxis,:,:]
         self.episode_steps = 0
+        self.agent_x = self.agent_start[0]
+        self.agent_y = self.agent_start[1]
 
         with open(self.analytics_dir+"//customLog.txt","a") as f:
             f.write("\n NEW TRAINING ITERATION CREATION")
