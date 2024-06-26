fa1ebe596c946964e43fb68fb6ae8e1d13e4597a
Aadi Kenchammana <104882666+Aadikenchammana@users.noreply.github.com>
2024-06-25 22:03:54 -0700

    yo

--- customSBenv_prob_baseline_benchmark_ppo_mip.py
+++ customSBenv_prob_baseline_benchmark_ppo_mip.py
@@ -475,7 +475,7 @@ if False:
     check_env(env)
     quit()
 # Instantiate the agent
-model = DQN("MlpPolicy", env, verbose=1)
+model = DQN("MultiInputPolicy", env, verbose=1)
 #model = PPO('MultiInputPolicy', env, verbose=1)
 """model_path = 'previous_models//og_ppo_acc+vel_weights.zip'
 model = PPO.load(model_path, env=env)"""
